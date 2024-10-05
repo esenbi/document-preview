00c4f5381af6a37e5f7f1f9d10e864b73b7cc4d3
Esen Biakhmet <kaltayev17@gmail.com>
2024-10-05 15:04:40 +0500

    add document preview

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Getting Started with Create React App
 
+React native document viewer for qollab app
+
+done only on Android, with this tuturial [Integrating React SPA or static HTML page into React Native with react-native-static-server and WebView](https://medium.com/@nik.usichenko/integrating-react-spa-or-static-html-page-into-react-native-with-react-native-static-server-and-d86cf9e3c562)
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+// import { pdf } from "./pdf";
 
 function App() {
+  const [docs, setDocs] = useState([
+    // { uri: "https://url-to-my-pdf.pdf" }, // Remote file
+    // { uri: pdf }, // Local File
+    // { uri: './example-files/file-sample_100kB.doc', fileType: 'doc' }, // Local File
+    // { uri: './example-files/file_example_PPT_250kB.ppt' }, // Local File
+  ])
+
+  useEffect(() => {
+    const showFile = (blob) => {
+      setDocs([{
+        uri: blob,
+      }])
+    }
+    window.showFile = showFile;
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>  
+      <h1>Preview document</h1>
+      <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} />
+    </>
   );
 }
 
